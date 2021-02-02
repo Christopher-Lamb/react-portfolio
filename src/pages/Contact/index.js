bce6baaa577a2b83c2a0d219b37210521b92a5ef
Chris Lamb <chris.lamb13@hotmail.com>
2021-02-01 21:15:04 -0500

    Contact Page Styling

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -3,6 +3,10 @@ import { Row, Col, Container } from "react-bootstrap";
 import NavBar from "../../components/NavBar";
 import Card from "../../components/Card";
 import "./style.css";
+import LinkedIn from "./assets/linkedIn.png";
+import Github from "./assets/Github.png";
+import Instagram from "./assets/instagram.jpeg";
+import Email from "./assets/mail.png";
 
 function Contact() {
   return (
@@ -14,21 +18,33 @@ function Contact() {
             <Card
               className={"cards-contact shadow-lg p-3 mb-5 bg-white rounded"}
             >
-              Linkedin
+              <a href="https://www.linkedin.com/">
+                <img id="linkedIn" src={LinkedIn} alt="LinkedIn" />
+              </a>
             </Card>
           </Col>
           <Col lg={4}>
             <Card
               className={"cards-contact shadow-lg p-3 mb-5 bg-white rounded"}
             >
-              Resume
+              <a href="resume">
+                <img id="email" src="" alt="No resume yet" />
+              </a>
             </Card>
           </Col>
           <Col lg={4}>
             <Card
               className={"cards-contact shadow-lg p-3 mb-5 bg-white rounded"}
             >
-              Email
+              <a href="">
+                <img
+                  action="mailto:chris.lamb13@hotmail.com"
+                  id="email"
+                  src={Email}
+                  alt="Email"
+                />
+              </a>
+              <p className="email-txt">Chris.lamb13@hotmail.com</p>
             </Card>
           </Col>
         </Row>
@@ -37,14 +53,18 @@ function Contact() {
             <Card
               className={"cards-contact shadow-lg p-3 mb-5 bg-white rounded"}
             >
-              GitHub
+              <a href="https://github.com/Christopher-Lamb">
+                <img id="github" src={Github} alt="Github" />
+              </a>
             </Card>
           </Col>
           <Col lg={4}>
             <Card
               className={"cards-contact shadow-lg p-3 mb-5 bg-white rounded"}
             >
-              Instagram
+              <a href="https://www.instagram.com/chris_lamb1713/">
+                <img id="instagram" src={Instagram} alt="Instagram" />
+              </a>
             </Card>
           </Col>
         </Row>
